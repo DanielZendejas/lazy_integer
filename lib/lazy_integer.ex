58946c3e439980bf97dee4bb8c4be5a386dacaa7
DanielZendejas <daniel8647@hotmail.com>
2016-12-13 10:02:26 -0600

    Cast empty string to null value

--- lib/lazy_integer.ex
+++ lib/lazy_integer.ex
@@ -4,6 +4,7 @@ defmodule LazyInteger do
 
   def cast(float) when is_float(float), do: {:ok, round(float)}
   def cast(int) when is_integer(int), do: {:ok, int}
+  def cast(string) when is_binary(string) and string == "", do: {:ok, nil}
   def cast(string) when is_binary(string) do
     case Integer.parse(string) do
       {int, _} -> {:ok, int}
